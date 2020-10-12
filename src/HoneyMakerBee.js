7101b0302744de470d8ce349f1474f06bcd44da9
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-12 11:39:08 -0700

    HoneyMakerBee complete

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Grub.call(this);
+  this.age = 10;
+  this.color = 'yellow';
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.job = 'make honey';
+
+HoneyMakerBee.prototype.honeyPot = 0;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  return this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  return this.honeyPot--;
+}
