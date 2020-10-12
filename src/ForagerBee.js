b4d795243671a19b40e02cdb084225846e2eb063
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-12 11:44:09 -0700

    Completed ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  Grub.call(this);
+  this.age = 10;
+  this.color = 'yellow';
 };
 
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.job = 'find pollen';
+
+ForagerBee.prototype.canFly = true;
+
+ForagerBee.prototype.treasureChest = [];
+
+ForagerBee.prototype.forage = function(treasure) {
+  return this.treasureChest.push(treasure);
+}
